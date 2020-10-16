a93ae7501ce52ca5baa6a3e14eaafaa1af584005
Aaron Amodt <aaron@amodttech.com>
2020-10-16 04:57:59 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,13 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   
-  
+  [
+    
+    
+    
+    
+    
+    ]
   
 end
 
